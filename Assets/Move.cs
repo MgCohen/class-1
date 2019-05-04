b80a1a8a6f443494f33588d5c0e236f8dcbe1fbe
Matheus Cohen <matheuscohen@hotmail.com>
2019-05-04 15:47:50 -0300

    Initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float speed;
+    public int points;
+
+
+    public void Update()
+    {
+        var pos = transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y -= Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y += Time.deltaTime * speed;
+        }
+        transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var circle = collision.GetComponent<CircleCollider2D>();
+        if (circle)
+        {
+            Destroy(circle.gameObject);
+            points += 1;
+            Debug.Log(points);
+        }
+    }
+}
---
+++ Assets/MoveNormalized.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveNormalized : MonoBehaviour
+{
+
+
+    public float speed;
+    public int points;
+
+    public void Update()
+    {
+        var pos = Vector3.zero; // ou new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x = 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y = -1;
+        }
+        transform.position += pos.normalized * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var circle = collision.GetComponent<CircleCollider2D>();
+        if (circle)
+        {
+            Destroy(circle.gameObject);
+            points += 1;
+            Debug.Log(points);
+        }
+    }
+}
